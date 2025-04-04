7809c1af3f4fa132e4dfb08b40ca7edc1d2b1a8d
rotat1ck <rotatick32151@gmail.com>
2025-04-04 14:26:28 +0300

    функция getCoctailPrice

--- app/utils/coctails.py
+++ app/utils/coctails.py
@@ -2,16 +2,19 @@ from flask import current_app, request, jsonify
 from app.models import Ingridient, Coctail
 
 def getCoctailPrice(content):
-    price = 
+    price = 0
     for ingridient in content:
         try:
             ingr = Ingridient.query.filter_by(name=ingridient['name']).first()
+            print(ingr.name + " " + ingridient['volume'])
             if not ingr:
                 return jsonify({"error": f"Ingridient {ingridient['name']} not found"}), 400
             if not ingridient['volume']:
                 return jsonify({"error": f"Ingridient {ingridient['name']} volume is required"}), 400
             
-
+            price += ingr.price * float(ingridient['volume'])
         except Exception as e:
             # удалить позже
             return jsonify({"error": f"Error parsing ingridients: {e}"}), 500
+        
+    return price
