0cabc4cf1f4b134f1a61e35e32c9c4b70a97de3a
rotat1ck <rotatick32151@gmail.com>
2025-04-04 14:14:28 +0300

    14:14

--- app/routes/users.py
+++ app/routes/users.py
@@ -84,10 +84,13 @@ def createCoctail(user):
     contentResult = ""
     for ingridient in content:
         try:
-            ingr = Ingridient.query.filter_by(name=ingridient).first()
+            ingr = Ingridient.query.filter_by(name=ingridient['name']).first()
             if not ingr:
-                return jsonify({"error": f"Ingridient {ingridient} not found"}), 404
-            contentResult += ingr.name + " "
+                return jsonify({"error": f"Ingridient {ingridient['name']} not found"}), 404
+            if not ingridient['volume']:
+                return jsonify({"error": f"Ingridient {ingridient['name']} volume is required"}),
+        
+            contentResult += ingr.name + " " + ingridient['volume'] + " "
         except Exception as e:
             # удалить позже
             return jsonify({"error": f"Error parsing ingridients: {e}"}), 500
