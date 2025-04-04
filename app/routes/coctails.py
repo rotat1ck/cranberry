4bf2677a5becb66ee63fc985ab815fc27d505229
rotat1ck <rotatick32151@gmail.com>
2025-04-04 18:34:08 +0300

    18:34

--- app/routes/coctails.py
+++ app/routes/coctails.py
@@ -60,53 +60,7 @@ def createCoctail(user):
     degreesResult = getCoctailDegree(content)
     if isinstance(degreesResult, tuple) and degreesResult[0].is_json:
         return degreesResult
-    ​
-2
-    degreesResult = getCoctailDegree(content)
-3
-    if isinstance(degreesResult, tuple) and degreesResult[0].is_json:
-4
-        return degreesResult
-5
     
-6
-    coctail = CustomCoctail(name=name, owner=user.id, glass=glass, description=description, price=priceResult
-7
-                    , content=contentResult, degrees=degreesResult)
-8
-    db.session.add(coctail)
-9
-    db.session.commit()
-10
-    
-11
-    return jsonify({"id": coctail.id, "name": coctail.name, "glass": coctail.glass,
-12
-                    "description": coctail.description, "price": coctail.price,
-13
-                    "content": coctail.content, "degrees": coctail.degrees}), 200
-14
-​
-15
-@coctails_bp.route('/deletecoctail/<int:coctailId>', methods=['DELETE'])    
-16
-@token_required
-17
-def deleteCoctail(user, coctailId):
-18
-    userCoctails = CustomCoctail.query.filter_by(id=coctailId).first()
-19
-    if not userCoctails:
-20
-        return jsonify({"error": "Coctail not found"}), 404
-21
-    
-22
-    db.session.delete(userCoctails)
-23
-    db.session.commit()
-24
-    return jsonify({"message": f"Coctail {userCoctails.name} deleted"}),
     coctail = CustomCoctail(name=name, owner=user.id, glass=glass, description=description, price=priceResult
                     , content=contentResult, degrees=degreesResult)
     db.session.add(coctail)
